008ac10219b3979d37e64f08f44dd64e6cb8de39
Sreeroop <adukkadu@hm.edu>
2023-02-05 00:39:09 +0100

    added comments

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ const color = require('colors');
 const input = process.argv[2];
 const langCode = franc(input);
 
-if (langCode === 'und') {
+if (langCode === 'und') { // und is the code for undefined
   console.log('Sorry, could not get the language of the text'.red);
 } else {
-  const language = langs.where('3', langCode);
-  console.log(`Our best guess is: ${language.name}`.green);
+  const language = langs.where('3', langCode); // 3 is the code for ISO 639-3
+  console.log(`Our best guess is: ${language.name}`.green); // greeen color
 }
 
 
